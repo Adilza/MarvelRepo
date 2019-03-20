ee4d9f356ae18f68dfd378030e3ed66cd80b3b59
Adilza Mattos <adilzamattos@gmail.com>
2019-03-20 16:44:05 -0300

    Novo commit

--- qafullstack/ruby/tipos.rb
+++ qafullstack/ruby/tipos.rb
@@ -14,7 +14,7 @@ puts disponivel
 quantidade = '9'
 
 
-puts produto.class 
-puts preco.class 
-puts quantidade.class 
-puts disponivel.class 
+# puts produto.class 
+# puts preco.class 
+# puts quantidade.class 
+# puts disponivel.class 
